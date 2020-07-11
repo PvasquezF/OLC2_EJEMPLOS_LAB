c38b2b2bd2434832728b4e4fcf3625f24510d99b
Pavel Vasquez <alexanderpavelv32@gmail.com>
2020-07-10 23:58:01 -0600

    ejemplo 1
    Se esta agregando interfaz, se piensa hacer una integración con gh pages para un acceso sencillo.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function ejemplo1() {
-    grammar.parse('evaluar[2+2];');
+    document.getElementById("salida").value = '';
+    var content = document.getElementById("entrada").value;
+    var result = grammar.parse(content);
+    document.getElementById("salida").value = result;
 }
-
-ejemplo1();
